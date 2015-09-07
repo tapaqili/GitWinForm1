26c2f26a386a0a2aad84f2b4fa2b5c09ef608025
tapqili <tapqili@gmail.com>
2015-09-07 16:48:02 +0200

    Added comments to form1.cs file

--- GitWinForm1/GitWinForm1/Form1.cs
+++ GitWinForm1/GitWinForm1/Form1.cs
@@ -10,6 +10,8 @@ using System.Windows.Forms;
 
 namespace GitWinForm1
 {
+    /*So I added this comment just for the sake of things*/
+    
     public partial class Form1 : Form
     {
         public Form1()
